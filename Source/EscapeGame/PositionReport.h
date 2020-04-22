c868d9a72c2a693f671e42ee921c80df7ba67b52
kenpos <kenpos13@gmail.com>
2020-04-22 21:53:28 +0900

    Add PositionReport

---
+++ Source/EscapeGame/PositionReport.h
@@ -0,0 +1,28 @@
+// copyright Kepos 2020
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "PositionReport.generated.h"
+
+
+UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+class ESCAPEGAME_API UPositionReport : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:	
+	// Sets default values for this component's properties
+	UPositionReport();
+
+protected:
+	// Called when the game starts
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+
+		
+};
