f8a1334a7e570a43b73db209ddd683e30d4ef885
kenpos <kenpos13@gmail.com>
2020-04-27 01:58:20 +0900

    Grabber

---
+++ Source/EscapeGame/Grabber.cpp
@@ -0,0 +1,52 @@
+// copyright Kepos 2020
+
+#include "EscapeGame.h"
+#include "Grabber.h"
+
+#define OUT
+
+// Sets default values for this component's properties
+UGrabber::UGrabber()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+	// ...
+}
+
+
+// Called when the game starts
+void UGrabber::BeginPlay()
+{
+	Super::BeginPlay();
+
+	UE_LOG(LogTemp, Warning, TEXT("Grabber reporting for duty"));
+	
+}
+
+
+// Called every frame
+void UGrabber::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	// Get player view point this tick
+	FVector PlayerViewPointLocation;
+	FRotator PlayerViewPointRotation;
+
+	GetWorld()->GetFirstPlayerController()->GetPlayerViewPoint(
+		OUT PlayerViewPointLocation,
+		OUT PlayerViewPointRotation
+	);
+	UE_LOG(LogTemp, Warning, TEXT("Lcation:%s, Position:%s"),
+		*PlayerViewPointLocation.ToString(),
+		*PlayerViewPointRotation.ToString());
+
+	// Ray-castout to reach distance
+
+	// See What we hit
+
+
+}
+
